13c91bb0c8ec8510748912fdfcffaa64491ad600
Mirkofilip <mirko.filippelli@gmail.com>
2023-11-02 20:52:45 +0100

    calendar page rev

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
     <div className="fullContainer">
       <SideMenu />
       <div className="dashboard">
+        <div className="breacrumb">Application / <span>Calendar</span></div>
+        <div className="borderHrBlue" aria-hidden="true"></div>
         <div className="row">
           <div className="col-md-12 title">
             <h2>Calendar</h2>
@@ -19,9 +21,9 @@ function App() {
           <div className="col-span-12 xl:col-span-4 2xl:col-span-3">
             <div className="col-span-12 box box-top">
               <button className="btn">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" icon-name="edit-3" class="lucide lucide-edit-3 w-4 h-4 mr-2" data-lucide="edit-3"><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 013 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg>
-                questo è un bottone</button>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam veniam voluptates fugit assumenda atque, quod accusamus consequatur temporibus adipisci laudantium qui molestiae numquam iste non asperiores illo. Odit, voluptate laudantium!
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" icon-name="edit-3" className="lucide lucide-edit-3 w-4 h-4 mr-2" data-lucide="edit-3"><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 013 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg>
+              Add New Schedule </button>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam veniam voluptates fugit assumenda atque, quod accusamus consequatur temporibus adipisci laudantium qui molestiae numquam iste non asperiores illo. Odit, voluptate laudantium!</p>  
             </div>
             <div className="col-span-12 box">
               <Datapicker />
--- src/components/sideMenu.js
+++ src/components/sideMenu.js
@@ -1,15 +1,19 @@
+import LogoHospital from '../hospital-svgrepo-com.svg'
+
 function SideMenu() {
     return(
         <div className="sideMenu">
             <div className="logo">
-            <h1>Fake Hospital</h1>
+                <img src={LogoHospital} alt="logo Fake Hospital" />
+                <h1>Fake Hospital</h1>
             </div>
-            <ul>
-                <li><a href="#0">lorem</a></li>
-                <li><a href="#0">lorem</a></li>
-                <li><a href="#0">lorem</a></li>
-                <li><a href="#0">lorem</a></li>
-            </ul>
+            <div className="borderHr" aria-hidden="true"></div>
+            <nav>
+                <ul>
+                    <li><a href="#0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" icon-name="home" data-lucide="home"><path d="M3 9l9-7 9 7v11a2 2 0 01-2 2H5a2 2 0 01-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>Dashboard</a></li>
+                    <li className="active"><a href="#0" ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" icon-name="calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>Calendar</a></li>
+                </ul>
+            </nav>
         </div>
     );
     
